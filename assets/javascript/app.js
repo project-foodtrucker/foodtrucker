88a2c4da8935e9fa45290f859cd7e69f823816cf
sternj20 <jessesternmusic@gmail.com>
2017-06-19 13:39:56 -0700

    changing variable username

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -204,8 +204,8 @@ $(document).ready(function() {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
     // The signed-in user info.
-    userName = result.user.email;
-
+    userName = firebase.auth().currentUser.email;
+    console.log(userName);
     // ...
   }).catch(function(error) {
     // Handle Errors here.
